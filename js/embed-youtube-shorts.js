0b1679e58a903a609ba01776a506a52f296b3408
karatejb <ppippenkimo@gmail.com>
2025-03-26 09:39:32 +0800

    Update shorts

--- js/embed-youtube-shorts.js
+++ js/embed-youtube-shorts.js
@@ -2,9 +2,9 @@
 
 <script type="text/javascript">
   // List of video IDs
-  const videoIds = ['Tn62WJHuV7I', 't4rXCvImxdw', 'Edw7iJYRjVk', 'zZg_YZK0HNo', 'yOCTcEvLtbU', 'z-cAahinPqo', 'YHtP2QB58pc', 'zVo2cirxVG8', 'oxky6LcFGtU', 'WG_mpFJjFdA', 'RfERaclPGCs', 'bRRA6cjVq0o', 'SvaBT6wwvPg', 'yTnXWLuFaj8', 'mIitkGrB16E', 'Hc6vWmQUnUs', '6Dhobsw5RHQ', 'tW-8ou2tzg0', 'xMWwQYaR918', '2DtQTdAfTf8', 'OyHPeB4gDVM', 'jMGIxK2t10E', 'LUrD04NZIJE', 'qvienk3TDW4', 'cpGAoUbnbPI', '_yebhB8jZng', 'AWohxclAtlw', 'hHkgO41oc38', 'XMmHOQbBmRI', 'VpmXwhGxvPE', 'cdUQ9uFbq-U', 'vS2bv5Re5Sg', 'EOkacxpccgI', 'VWKcb_KfgqU', 'nKKHBppFElw', 'o879IUwlDnA', 'XKYY2oV_jY4'];
+  const videoIds = ['Sx0-1msKP3Q', 'NDcB3wlcC5k', 'cSnDYH5xk0U', 'wNy3nfE1MFQ', 'OXFhYuZsJMU', 'Tn62WJHuV7I', 't4rXCvImxdw', 'Edw7iJYRjVk', 'zZg_YZK0HNo', 'yOCTcEvLtbU', 'z-cAahinPqo', 'YHtP2QB58pc', 'zVo2cirxVG8', 'oxky6LcFGtU', 'WG_mpFJjFdA', 'RfERaclPGCs', 'bRRA6cjVq0o', 'SvaBT6wwvPg', 'yTnXWLuFaj8', 'mIitkGrB16E', 'Hc6vWmQUnUs', '6Dhobsw5RHQ', 'tW-8ou2tzg0', 'xMWwQYaR918', '2DtQTdAfTf8', 'OyHPeB4gDVM', 'jMGIxK2t10E'];
 
-  const archivedIds = [];
+  const archivedIds = ['LUrD04NZIJE', 'qvienk3TDW4', 'cpGAoUbnbPI', '_yebhB8jZng', 'AWohxclAtlw', 'hHkgO41oc38', 'XMmHOQbBmRI', 'VpmXwhGxvPE', 'cdUQ9uFbq-U', 'vS2bv5Re5Sg', 'EOkacxpccgI', 'VWKcb_KfgqU', 'nKKHBppFElw', 'o879IUwlDnA', 'XKYY2oV_jY4'];
 
   // Select a random video ID
   const randomVideoId = videoIds[Math.floor(Math.random() * videoIds.length)];
