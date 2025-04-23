431cececb7632419e8ee99da0790a887d566318e
karatejb <ppippenkimo@gmail.com>
2025-04-23 09:35:32 +0800

    Update shorts

--- js/embed-youtube-shorts.js
+++ js/embed-youtube-shorts.js
@@ -2,7 +2,7 @@
 
 <script type="text/javascript">
   // List of video IDs
-  const videoIds = ['1PyktfzoGf4', 'jQEG__stTYE', 'ynzSZVZDG_c', 'DdasqGZWoF0', 'Sx0-1msKP3Q', 'NDcB3wlcC5k', 'cSnDYH5xk0U', 'wNy3nfE1MFQ', 'OXFhYuZsJMU', 'Tn62WJHuV7I', 't4rXCvImxdw', 'Edw7iJYRjVk', 'zZg_YZK0HNo', 'yOCTcEvLtbU', 'z-cAahinPqo', 'YHtP2QB58pc', 'zVo2cirxVG8', 'oxky6LcFGtU', 'WG_mpFJjFdA', 'RfERaclPGCs', 'bRRA6cjVq0o', 'SvaBT6wwvPg', 'yTnXWLuFaj8', 'mIitkGrB16E', 'Hc6vWmQUnUs', '6Dhobsw5RHQ', 'tW-8ou2tzg0', 'xMWwQYaR918', '2DtQTdAfTf8', 'OyHPeB4gDVM', 'jMGIxK2t10E'];
+  const videoIds = ['SF7uZayMtck', 'i3u9AsQajuY', 'x5djGqUvXNk', '3SKu-GU8ncw', 'jeZpnGyTvb0', 'a1yj9p27fY', '1PyktfzoGf4', 'jQEG__stTYE', 'ynzSZVZDG_c', 'DdasqGZWoF0', 'Sx0-1msKP3Q', 'NDcB3wlcC5k', 'cSnDYH5xk0U', 'wNy3nfE1MFQ', 'OXFhYuZsJMU', 'Tn62WJHuV7I', 't4rXCvImxdw', 'Edw7iJYRjVk', 'zZg_YZK0HNo', 'yOCTcEvLtbU', 'z-cAahinPqo', 'YHtP2QB58pc', 'zVo2cirxVG8', 'oxky6LcFGtU', 'WG_mpFJjFdA', 'RfERaclPGCs', 'bRRA6cjVq0o', 'SvaBT6wwvPg', 'yTnXWLuFaj8', 'mIitkGrB16E', 'Hc6vWmQUnUs', '6Dhobsw5RHQ', 'tW-8ou2tzg0', 'xMWwQYaR918', '2DtQTdAfTf8', 'OyHPeB4gDVM', 'jMGIxK2t10E'];
 
   const archivedIds = ['LUrD04NZIJE', 'qvienk3TDW4', 'cpGAoUbnbPI', '_yebhB8jZng', 'AWohxclAtlw', 'hHkgO41oc38', 'XMmHOQbBmRI', 'VpmXwhGxvPE', 'cdUQ9uFbq-U', 'vS2bv5Re5Sg', 'EOkacxpccgI', 'VWKcb_KfgqU', 'nKKHBppFElw', 'o879IUwlDnA', 'XKYY2oV_jY4'];
 
